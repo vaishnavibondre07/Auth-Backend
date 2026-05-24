0794c35c73393a81bd78d6eed777b5f968303b70
vaishnavibondre07 <vaishnavibondre07@gmail.com>
2026-05-24 21:03:30 +0530

    fix: Update CORS configuration to allow requests from the production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-  origin: [ "http://localhost:5173", ],
+  origin: [ "http://localhost:5173", "https://authentication-system-beryl.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
