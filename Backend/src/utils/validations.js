0ff043ff2d48d22cc1946a69e193232f76c56f9e
vaishnavibondre07 <vaishnavibondre07@gmail.com>
2026-05-26 18:04:48 +0530

    Improved Validations

--- Backend/src/utils/validations.js
+++ Backend/src/utils/validations.js
@@ -1,15 +1,15 @@
 export function validateUsername(username){
-    const usernamePattern=/^[A-Za-z]{3,}$/;
+    const usernamePattern=/^[A-Za-z]{3,20}$/;
     return usernamePattern.test(username);
 }
 
-export function validateEmail(email){
-   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   return emailPattern.test(email)
+export function validateEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  return emailPattern.test(email);
 }
 
-export function validatePassword(password){
-    const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/;
-    return passwordPattern.test(password)
+export function validatePassword(password) {
+  const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*\-_]).{8,20}$/;
+  return passwordPattern.test(password);
 }
 
